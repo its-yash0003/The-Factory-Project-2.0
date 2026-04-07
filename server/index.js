e86c8da31500924cc9f9b55b4cf0f7491fd882ce
YASH GAUTAM <152496325+its-yash0003@users.noreply.github.com>
2026-04-07 23:48:33 +0530

    updated vercel link in index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-app-name.vercel.app'],
+  origin: ['http://localhost:5173', 'https://the-factory-project-2-0.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
